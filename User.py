c905e3bd6db0bc3178552eb710ffddc29c24aae1
avishagMinnes <157994250+avishagMinnes@users.noreply.github.com>
2024-02-27 16:40:30 +0200

    Update User.py

--- User.py
+++ User.py
@@ -1,4 +1,4 @@
-from Lib.Post import Post
+from Post import PostFactory
 
 
 class User():
@@ -6,44 +6,49 @@ class User():
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.logged = true
+        self.logged = True
         self.following = []
         self.posts = []
         self.followers = []
         self.notifications = []
 
     def print_notifications(self):
+        print(f"{self.username}'s notifications:")
         for notification in self.notifications:
             print(notification)
 
-    def login(self, username, password):
-        logged = true;
+    def login(self):
+        self.logged = True;
 
-    def logout(self, username):
-        logged = false;
+    def logout(self):
+        self.logged = False;
 
     def follow(self, user):
-        while(self.logged):
+        if self.logged:
             if user.username not in self.following:
                 self.following.append(user.username)
                 print(self.username + " started following " + user.username)
                 user.followers.append(self)
 
     def unfollow(self, user):
-        while(self.logged):
+        if self.logged:
+            if user.username not in self.following:
+                return
             self.following.remove(user.username)
             print(self.username + " unfollowed " + user.username)
             user.followers.remove(self)
 
-    def publish_post(self, type, *args, **kwargs):
-        while(self.logged):
-            self.posts.append(PostFactory.create_post(type, *args, **kwargs))
-            for follower in self.followers:
-                follower.notifications.append(self.username + " has a new post")
-            return PostFactory.create_post(type, *args, **kwargs)
+    def publish_post(self, post_type, *args, **kwargs):
+        new_post = PostFactory.create_post(self, post_type, *args, **kwargs)
+        self.posts.append(new_post)
+
+        for follower in self.followers:
+            follower.notifications.append(self.username + " has a new post")
+
+        return new_post
 
     def __str__(self):
-        return f"User name: {self.username}, Number of posts: {len(self.posts)}, Number of followers: {len(self.followers)}\n"
+        return f"User name: {self.username}, Number of posts: {len(self.posts)}, Number of followers: {len(self.followers)}"
 
 
 
